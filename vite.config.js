3a3d73d76b1fe1209f0c883a67fee8fa6791ef2e
M. Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2025-09-15 11:10:42 +0600

    git pages updated

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	base: "/Agency-Landing-Page"
+	base: '/Agency-Landing-Page/',
 });
